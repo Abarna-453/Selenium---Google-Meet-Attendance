c8e0b37132cfb4e48c517a353afea323eaf2a9e5
Abarna-453 <abarnask2003@gmail.com>
2022-01-27 15:08:38 +0530

    All done

--- Google-Meet-Attendance-using-Python-master/Attendance.py
+++ Google-Meet-Attendance-using-Python-master/Attendance.py
@@ -5,22 +5,15 @@ import os
 import time
 from time import sleep
 
-# excel file imports
-from openpyxl import load_workbook
-from openpyxl.utils import column_index_from_string
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-
 
 # The function directly installs the required libraries
 def install(package):
     os.system("pip install " + str(package))
     print("Installed", package.upper())
 
-
 if __name__ == '__main__':
-    install("openpyxl")
     install("selenium")
     gmail_id = input('Enter your gmail id: ')
     gmail_password = input('Enter your gmail password: ')
@@ -30,28 +23,33 @@ if __name__ == '__main__':
     opt = Options()
     opt.add_argument("--disable-infobars")
     opt.add_argument("start-maximized")
-    opt.add_argument("--disable-extensions")
+    opt.add_extension('C:\\webdriver\\attendance_tracker.crx')
 
     # This part allows the notifications, mic and camera permissions
     # Pass the argument 1 to allow and 2 to block
     opt.add_experimental_option("prefs", {
         "profile.default_content_setting_values.media_stream_mic": 1,
-        "profile.default_content_setting_values.media_stream_camera": 1 ,
+        "profile.default_content_setting_values.media_stream_camera": 1,
         "profile.default_content_setting_values.geolocation": 1,
         "profile.default_content_setting_values.notifications": 1
-      })
+    })
 
     # Sign in to google
-    driver = webdriver.Chrome(options=opt, executable_path="C:\webdriver\chromedriver.exe") # eg. r"C:\Users\hp\Downloads\chromedriver_win32\chromedriver.exe"
-    # driver.get('https://stackoverflow.com/users/signup?ssrc=head&returnurl=%2fusers%2fstory%2fcurrent%27')  # signing in to google through stack overflow
-    # sleep(2)
+    driver = webdriver.Chrome(options=opt,
+                              executable_path="C:\webdriver\chromedriver.exe")  # eg. r"C:\Users\hp\Downloads\chromedriver_win32\chromedriver.exe"
     driver.get('https://myaccount.google.com/')
     driver.find_element_by_xpath('/html/body/header/div[1]/div[5]/ul/li[2]/a').click()  # signing in with google
-    driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input').send_keys(gmail_id)  # entering the gmail id
-    driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/span').click()
+    driver.find_element_by_xpath(
+        '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input').send_keys(
+        gmail_id)  # entering the gmail id
+    driver.find_element_by_xpath(
+        '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/span').click()
     sleep(2)
-    driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input').send_keys(gmail_password)  # entering the password
-    driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/span').click()
+    driver.find_element_by_xpath(
+        '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input').send_keys(
+        gmail_password)  # entering the password
+    driver.find_element_by_xpath(
+        '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/span').click()
     sleep(2)
 
     driver.get('https://meet.google.com/')
@@ -60,31 +58,31 @@ if __name__ == '__main__':
     # Case when logged in with personal gmail account
     driver.find_element_by_css_selector('input#i3').send_keys(meet_link)  # Enter a code or link
     sleep(1)
-    driver.find_element_by_css_selector('button.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.ksBjEc.lKxP2d.cjtUbb').click()  # join
+    driver.find_element_by_css_selector(
+        'button.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.ksBjEc.lKxP2d.cjtUbb').click()  # join
     sleep(1)
-    driver.find_element_by_xpath('/html/body/div[1]/c-wiz/div/div/div[9]/div[3]/div/div/div[3]/div/div/div[1]/div[1]/div/div[4]/div[1]/div/div/div').click()
-    driver.find_element_by_xpath('/html/body/div[1]/c-wiz/div/div/div[9]/div[3]/div/div/div[3]/div/div/div[1]/div[1]/div/div[4]/div[2]/div/div').click()
+    driver.find_element_by_xpath(
+        '/html/body/div[1]/c-wiz/div/div/div[9]/div[3]/div/div/div[3]/div/div/div[1]/div[1]/div/div[4]/div[1]/div/div/div').click()
+    driver.find_element_by_xpath(
+        '/html/body/div[1]/c-wiz/div/div/div[9]/div[3]/div/div/div[3]/div/div/div[1]/div[1]/div/div[4]/div[2]/div/div').click()
     sleep(2)
-
     driver.find_element_by_css_selector('div.uArJ5e.UQuaGc.Y5sE8d.uyXBBb.xKiqt').click()  # join now
     sleep(2)
-    driver.find_element_by_xpath('/html/body/div[1]/c-wiz/div[1]/div/div[9]/div[3]/div[10]/div[3]/div[3]/div/div/div[2]/span/button').click()  # participant list
+    driver.find_element_by_xpath(
+        '/html/body/div[1]/c-wiz/div[1]/div/div[9]/div[3]/div[10]/div[3]/div[3]/div/div/div[2]/span/button').click()  # participant list
     sleep(1)
-    driver.get_screenshot_as_file("screenshot.png")
-    names = driver.find_elements_by_css_selector('span.ZjFb7c')  # participantsspan.ZjFb7c
-    for e in names[1:]:
-        print(e.text)
+    driver.find_element_by_xpath(
+        '/html/body/div[1]/c-wiz/div[1]/div/div[9]/div[3]/div[10]/div[1]/div/div/div/button').click()
+    sleep(4)
+    driver.find_element_by_xpath(
+        '/html/body/div[1]/c-wiz/div[1]/div/div[9]/div[3]/div[10]/div[1]/div/div/div/button').click()
+    chwnd = driver.window_handles[1]
+    driver.switch_to.window(chwnd)
 
-    #n = int(driver.find_element_by_css_selector('div.eUyZxf span.rua5Nb').text.strip('(').strip(')'))  # no. of participants present
+    sleep(5)
+    driver.find_element_by_xpath('/html/body/div[1]/div[3]/table[1]/tbody[1]/tr/td[8]/a/button').click()
+    sleep(3)
+    driver.find_element_by_xpath('/html/body/div/div[1]/div[2]/div[1]/button').click()
+    sleep(3)
 
-    wb = load_workbook('Google_Attendance.xlsx')
-    sheet = wb['Attendance Sheet']
-
-    for name in names:
-        for box in range(2, sheet.max_row+1):
-            if name.text.lower() == (sheet.cell(row=box, column=column_index_from_string('A')).value.lower()):  # checks if the participant is present in the sheet
-                sheet[f'B{box}'] = 'Present'  # marks present of that participant
-
-    wb.save('Google_Attendance.xlsx')
-    # print(f'No. of participants : {n-1}')  # 1 participant adds up while taking attendance
     print('Attendance taken successfully!')
